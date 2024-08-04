169c9269b5e23f0bead62fbcdaa07c9e74c9a71e
Vitória Cristina do Nascimento <105313328+vitoria33@users.noreply.github.com>
2024-08-04 12:02:10 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ const perguntas = [
         alternativas:  [
             {
                 texto: "gosto de cuidar da minha saúde",
-                afirmacao: "Você provavelmete gosta de exercícios físicos "
+                afirmacao:"Provavelmete gosta de exercícios físicos "
             },
             {
                 texto: "cuido por obrigação",
-                afirmacao: "Você deve odiar exercícios físicos"
+                afirmacao: "Deve odiar exercícios físicos"
             }
         ]
     },
@@ -23,11 +23,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "de 4 à 5 vezes por semana",
-                afirmacao: "Parabéns você é guerreiro"
+                afirmacao: "É guerreiro"
             },
             {
                 texto: "de 1 à 3 vezes por semana",
-                afirmacao: "Você é um preguiçoso"
+                afirmacao: "É um preguiçoso"
             }
         ]
     },
@@ -35,12 +35,12 @@ const perguntas = [
         enunciado: "Sua rotina de sono é boa?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Sim",
+                afirmacao: "Dorme de 8 à 10 horas por dia"
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "Não",
+                afirmacao: "Dorme de 5 à 7 horas "
             }
         ]
     },
@@ -48,25 +48,25 @@ const perguntas = [
         enunciado: "Gosta de caminhar ou corre?",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "Caminhar",
+                afirmacao: "Gosta de cardio mais tranquilo"
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "Correr",
+                afirmacao: "Gosta de cardio mais agitado"
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "Você prefere um treino tranquilo ou um treino pesado?",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "Treino pesado",
+                afirmacao: "Vai embora parecendo outra pessoa descabelado(a) e suando muito"
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Treino tranquilo",
+                afirmacao:"Vai embora do mesmo jeito que chegou"
             }
         ]
     },
@@ -105,7 +105,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "Você...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
